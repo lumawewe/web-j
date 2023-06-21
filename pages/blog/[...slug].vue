69f54d7dd855d9391dbf07432d2cd87b5ad29cc8
Luciano T <tonetlds@gmail.com>
2023-06-21 15:43:37 -0300

    Created a section with title, date, author, and category at the top of the blog post view

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,16 +1,13 @@
 <template>
 	<NuxtLayout>
 		<article class="prose mx-auto w-full max-w-4xl py-24 px-6 lg:px-8">
-			<ContentDoc />
-
 			<section class="mt-12">
-				<hr>
 				<div class="flex items-start">
-					<img :src="data.author.image"
-						class="w-12 h-12 rounded-full my-1" />
+					<h1 class="text-3xl font-bold">{{ data.title }}</h1>
 					<small class="ml-4">
-						<h3 class="text-xl m-0 mb-1 font-semibold">{{ data.author.name }}</h3>
-						<p class="text-gray-500 max-w-sm leading-tight">{{ data.author.description }}</p>
+						<p class="text-gray-500 max-w-sm leading-tight">{{ formatDate(data.date, 'pt-BR') }}</p>
+						<p class="text-gray-500 max-w-sm leading-tight">{{ data.author.name }}</p>
+						<p class="text-gray-500 max-w-sm leading-tight">{{ data.category }}</p>
 					</small>
 				</div>
 			</section>
@@ -23,11 +20,15 @@
 						alt="{{ data.title }}" />
 				</div>
 			</section>
+			<ContentDoc />
 			<section class="mt-12">
 				<hr>
 				<div class="flex items-start">
+					<img :src="data.author.image"
+						class="w-12 h-12 rounded-full my-1" />
 					<small class="ml-4">
-						<p class="text-gray-500 max-w-sm leading-tight">{{ formatDate(data.date, 'pt-BR') }}</p>
+						<h3 class="text-xl m-0 mb-1 font-semibold">{{ data.author.name }}</h3>
+						<p class="text-gray-500 max-w-sm leading-tight">{{ data.author.description }}</p>
 					</small>
 				</div>
 			</section>
